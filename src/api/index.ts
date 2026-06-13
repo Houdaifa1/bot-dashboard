22a0e1068d4823f15e5abada185c741e5f9f29da
houdaifa drahm <houdaifadrahm@192.168.1.4>
2026-06-13 08:56:39 +0100

    hdrahm<jun 13>: fix doctor and speciality deletion

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://api.houdaifa.dev'
 console.log('API URL:', BASE_URL)
 export const api = axios.create({ baseURL: BASE_URL })
 
